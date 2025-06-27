0cf2494c44bfbd80abf84eb737bd6ad563cf5e8f
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-27 19:53:50 -0300

    projeto base com spring web, spring dev tools e spring starter test

---
+++ src/main/java/br/com/kaiomuniz/ProdutoTesteProvaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaiomuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoTesteProvaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoTesteProvaApiApplication.class, args);
+	}
+
+}
